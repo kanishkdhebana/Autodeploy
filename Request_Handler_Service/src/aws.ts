2c7e8e85305fc87658562e826420144e8a3a8e5c
kanishkdhebana <kanishkdhebana@Wednesday.local>
2025-11-01 18:54:21 +0530

    Line changes are just removal of tab, nothing crazy, and delete functionality on sqs is working for now.

--- Request_Handler_Service/src/aws.ts
+++ Request_Handler_Service/src/aws.ts
@@ -1,37 +1,44 @@
-import { GetObjectCommand, type GetObjectCommandOutput, S3Client } from "@aws-sdk/client-s3";
+import {
+  GetObjectCommand,
+  type GetObjectCommandOutput,
+  S3Client,
+} from "@aws-sdk/client-s3";
 
 function getS3Client() {
-    const region = process.env.AWS_REGION ;
-    const accessKeyId = process.env.AWS_ACCESS_KEY_ID ;
-    const secretAccessKey = process.env.AWS_SECRET_ACCESS_KEY ;
-    
-    if (!region || !accessKeyId || !secretAccessKey) {
-        throw new Error("Missing AWS credentials or region in environment variables.") ;
-    }
+  const region = process.env.AWS_REGION;
+  const accessKeyId = process.env.AWS_ACCESS_KEY_ID;
+  const secretAccessKey = process.env.AWS_SECRET_ACCESS_KEY;
 
-    return new S3Client({
-        region,
-        credentials: {
-            accessKeyId,
-            secretAccessKey
-        }
-    }) ;
+  if (!region || !accessKeyId || !secretAccessKey) {
+    throw new Error(
+      "Missing AWS credentials or region in environment variables.",
+    );
+  }
+
+  return new S3Client({
+    region,
+    credentials: {
+      accessKeyId,
+      secretAccessKey,
+    },
+  });
 }
 
-
 export async function getFile(fileName: string) {
-    const s3 = getS3Client() ;
-    const bucketName = process.env.AWS_BUCKET_NAME ;
+  const s3 = getS3Client();
+  const bucketName = process.env.AWS_BUCKET_NAME;
 
-    if (!bucketName) {
-        throw new Error("Aws bucket name is missing from environment vairables") ;
-    }
-    
-    const params = {
-        Bucket: bucketName,
-        Key: fileName
-    } ;
+  if (!bucketName) {
+    throw new Error("Aws bucket name is missing from environment vairables");
+  }
 
-    const contents: GetObjectCommandOutput = await s3.send(new GetObjectCommand(params)) ;
-    return contents ;
+  const params = {
+    Bucket: bucketName,
+    Key: fileName,
+  };
+
+  const contents: GetObjectCommandOutput = await s3.send(
+    new GetObjectCommand(params),
+  );
+  return contents;
 }
--- Request_Handler_Service/src/index.ts
+++ Request_Handler_Service/src/index.ts
@@ -1,41 +1,41 @@
 import dotenv from "dotenv";
 dotenv.config();
 
-import express from "express" ;
-import { Readable } from 'stream';
-import { getFile } from "./aws.js"
+import express from "express";
+import { Readable } from "stream";
+import { getFile } from "./aws.js";
 
 const app = express();
 
-app.get('/*', async (req, res) => {
-    const host = req.hostname ;
-    const id = host.split(".")[0] ;
-    const filePath = req.path ;
+app.get("/*", async (req, res) => {
+  const host = req.hostname;
+  const id = host.split(".")[0];
+  const filePath = req.path;
 
-    try {
-        const contents = await getFile(`build/${id}${filePath}`) ;
+  try {
+    const contents = await getFile(`build/${id}${filePath}`);
 
-        console.log("done") ;
-        
-        const type = filePath.endsWith("html") ? 
-                    "text/html" : filePath.endsWith("css") ?
-                    "text/css" : "application/javascript" ;
+    console.log("done");
 
-        res.set("Content-Type", type);
+    const type = filePath.endsWith("html")
+      ? "text/html"
+      : filePath.endsWith("css")
+        ? "text/css"
+        : "application/javascript";
 
-        if (contents.Body && contents.Body instanceof Readable) {
-            Readable.from(contents.Body).pipe(res);
-        } else {
-            res.send(contents.Body);
-        }
+    res.set("Content-Type", type);
 
-    } catch (error) {
-        console.error("Error fetching file from S3:", error) ;
-        res.status(500).send("Internal Server Error") ;
+    if (contents.Body && contents.Body instanceof Readable) {
+      Readable.from(contents.Body).pipe(res);
+    } else {
+      res.send(contents.Body);
     }
+  } catch (error) {
+    console.error("Error fetching file from S3:", error);
+    res.status(500).send("Internal Server Error");
+  }
 });
 
-app.listen(3001) ;
-
+app.listen(3001);
 
 // x.vercel.com/index.css
